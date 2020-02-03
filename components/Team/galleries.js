e1a9c275c5ff6148d3711f6ee5df59421e962f6d
Jan Rádl <jan.radl@citrix.com>
2020-02-03 14:07:15 +0100

    New photos

--- components/Team/galleries.js
+++ components/Team/galleries.js
@@ -140,6 +140,306 @@ export const galleries = {
         src: "/static/photos/galleries/voky/p-28.jpg",
         w: 1920,
         h: 2560
+      },
+      {
+        src: "/static/photos/galleries/voky/p-29.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-30.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-31.jpg",
+        w: 1300,
+        h: 975
+      },
+      {
+        src: "/static/photos/galleries/voky/p-32.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-33.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-34.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-35.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-36.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-37.jpg",
+        w: 1300,
+        h: 2104
+      },
+      {
+        src: "/static/photos/galleries/voky/p-38.jpg",
+        w: 1300,
+        h: 1101
+      },
+      {
+        src: "/static/photos/galleries/voky/p-39.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-40.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-41.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-42.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-43.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-44.jpg",
+        w: 1300,
+        h: 1949
+      },
+      {
+        src: "/static/photos/galleries/voky/p-45.jpg",
+        w: 1300,
+        h: 1733
+      },
+      {
+        src: "/static/photos/galleries/voky/p-46.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-47.jpg",
+        w: 974,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-48.jpg",
+        w: 1300,
+        h: 974
+      },
+      {
+        src: "/static/photos/galleries/voky/p-49.jpg",
+        w: 1300,
+        h: 1733
+      },
+      {
+        src: "/static/photos/galleries/voky/p-50.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-51.jpg",
+        w: 1300,
+        h: 1733
+      },
+      {
+        src: "/static/photos/galleries/voky/p-52.jpg",
+        w: 689,
+        h: 728
+      },
+      {
+        src: "/static/photos/galleries/voky/p-53.jpg",
+        w: 1300,
+        h: 2112
+      },
+      {
+        src: "/static/photos/galleries/voky/p-54.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-55.jpg",
+        w: 1300,
+        h: 1571
+      },
+      {
+        src: "/static/photos/galleries/voky/p-56.jpg",
+        w: 1300,
+        h: 1202
+      },
+      {
+        src: "/static/photos/galleries/voky/p-57.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-58.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-59.jpg",
+        w: 1300,
+        h: 975
+      },
+      {
+        src: "/static/photos/galleries/voky/p-60.jpg",
+        w: 1300,
+        h: 1733
+      },
+      {
+        src: "/static/photos/galleries/voky/p-61.jpg",
+        w: 1300,
+        h: 975
+      },
+      {
+        src: "/static/photos/galleries/voky/p-62.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-63.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-64.jpg",
+        w: 1300,
+        h: 1733
+      },
+      {
+        src: "/static/photos/galleries/voky/p-65.jpg",
+        w: 1300,
+        h: 1919
+      },
+      {
+        src: "/static/photos/galleries/voky/p-66.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-67.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-68.jpg",
+        w: 1300,
+        h: 1733
+      },
+      {
+        src: "/static/photos/galleries/voky/p-69.jpg",
+        w: 975,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-70.jpg",
+        w: 1300,
+        h: 975
+      },
+      {
+        src: "/static/photos/galleries/voky/p-71.jpg",
+        w: 1300,
+        h: 1733
+      },
+      {
+        src: "/static/photos/galleries/voky/p-72.jpg",
+        w: 1300,
+        h: 1733
+      },
+      {
+        src: "/static/photos/galleries/voky/p-73.jpg",
+        w: 1300,
+        h: 1429
+      },
+      {
+        src: "/static/photos/galleries/voky/p-74.jpg",
+        w: 974,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-75.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-76.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-77.jpg",
+        w: 1300,
+        h: 974
+      },
+      {
+        src: "/static/photos/galleries/voky/p-78.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-79.jpg",
+        w: 960,
+        h: 720
+      },
+      {
+        src: "/static/photos/galleries/voky/p-80.jpg",
+        w: 974,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-81.jpg",
+        w: 1300,
+        h: 2150
+      },
+      {
+        src: "/static/photos/galleries/voky/p-82.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-83.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-84.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-85.jpg",
+        w: 1300,
+        h: 2367
+      },
+      {
+        src: "/static/photos/galleries/voky/p-86.jpg",
+        w: 1300,
+        h: 1300
+      },
+      {
+        src: "/static/photos/galleries/voky/p-88.jpg",
+        w: 960,
+        h: 818
+      },
+      {
+        src: "/static/photos/galleries/voky/p-89.jpg",
+        w: 1300,
+        h: 1300
       }
     ]
   },
