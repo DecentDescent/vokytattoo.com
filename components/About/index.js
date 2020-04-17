6dc93795cbb269add210ba910f9568c92e5a568f
Jan Rádl <janradl@Jan-MacBook-Pro-2.local>
2020-04-17 10:32:21 +0200

    Disclaimer

--- components/About/index.js
+++ components/About/index.js
@@ -4,28 +4,44 @@ import classnames from "classnames";
 
 const About = () => {
   return (
-    <section
-      className={classnames(styles["section"], styles["section--about"])}
-      id="about"
-    >
-      <div
-        className={classnames(styles["container"], styles["container--center"])}
+    <>
+      <section
+        className={classnames(styles["section"], styles["section--disclaimer"])}
       >
-        <TranslateContext.Consumer>
-          {s => {
-            return (
-              <>
-                <h1>{s["aboutTitle"]}</h1>
-                <p>{s["aboutLine1"]}</p>
-                <p>{s["aboutLine2"]}</p>
-                <p>{s["aboutLine3"]}</p>
-                <p>{s["aboutLine4"]}</p>
-              </>
-            );
-          }}
-        </TranslateContext.Consumer>
-      </div>
-    </section>
+        <div className={styles["disclaimer"]}>
+          Vážení zákazníci, stále máme v této nelehké koronavirové době zavřeno.
+          Zatím to vypadá, že se Vám budeme moci věnovat od 8. června. Tak se
+          zatím mějte fajn, pevné zdraví a noste roušky!
+          <br /> <br />
+          Voky, Vojta a Jirka
+        </div>
+      </section>
+      <section
+        className={classnames(styles["section"], styles["section--about"])}
+        id="about"
+      >
+        <div
+          className={classnames(
+            styles["container"],
+            styles["container--center"]
+          )}
+        >
+          <TranslateContext.Consumer>
+            {(s) => {
+              return (
+                <>
+                  <h1>{s["aboutTitle"]}</h1>
+                  <p>{s["aboutLine1"]}</p>
+                  <p>{s["aboutLine2"]}</p>
+                  <p>{s["aboutLine3"]}</p>
+                  <p>{s["aboutLine4"]}</p>
+                </>
+              );
+            }}
+          </TranslateContext.Consumer>
+        </div>
+      </section>
+    </>
   );
 };
 
